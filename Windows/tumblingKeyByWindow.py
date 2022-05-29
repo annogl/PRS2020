6d33af60c5480f48b4056fa94ba5145553b64cd9
marcin witkowski <marcin.witkowski@e-qsg.com>
2022-05-29 22:41:10 +0200

    Classes commit

--- Windows/tumblingKeyByWindow.py
+++ Windows/tumblingKeyByWindow.py
@@ -1,7 +1,6 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment
-from pyflink.table.expressions import lit, col
-from pyflink.table.window import Tumble
+
 
 def main():
     env = StreamExecutionEnvironment.get_execution_environment()
@@ -11,16 +10,14 @@ def main():
 
     env.add_jars("file:///home/marcin/PycharmProjects/FlinkClasses/flink-sql-connector-kafka-1.15.0.jar")
 
+    # nie możemy dodać daty :/
     src_ddl = """
         CREATE TABLE sensor (
-            event_time TIMESTAMP(3) METADATA FROM 'timestamp',
-            WATERMARK FOR event_time AS event_time,
             temp DOUBLE,
             moisture DOUBLE,
             light INT,
             conductivity INT,
-            battery INT,
-            proctime AS PROCTIME()
+            battery INT
             ) WITH (
             'connector' = 'kafka',
             'topic' = 'xiaomi_json',
@@ -39,21 +36,14 @@ def main():
     print('\nSchema')
     tbl.print_schema()
 
-    # tbl.execute().print()
+    data_stream = t_env.to_data_stream(tbl)
 
-    windowed_rev = tbl.window(Tumble.over(lit(10).minutes)\
-                              .on(tbl.event_time)
-                              .alias('w')) \
-        .group_by(col('w')) \
-        .select(tbl.temp.avg.alias('temp_avg'),
-                col('w').start.alias('window_start'),
-                col('w').end.alias('window_end'))
+    windowed_rev = data_stream.key_by(lambda a: a[1]).print()
+                              # nie mozemy dodac window
+                              #.window() # ????
+                              #.process() # ????
 
-
-    print('\nSchema')
-    windowed_rev.print_schema()
-
-    windowed_rev.execute().print()
+    env.execute()
 
 if __name__ == '__main__':
     main()
